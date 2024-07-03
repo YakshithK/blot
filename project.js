ead393a39aa09f30465c313b3cc7953e576d5ab6
Yakshith <yakshith.kommineni@gmail.com>
2024-07-02 23:29:52 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,16 @@
+// welcome to blot!
+
+// check out this guide to learn how to program in blot
+// https://blot.hackclub.com/editor?guide=start
+
+const width = 270;
+const height = 270;
+
+setDocDimensions(width, height);
+
+drawLines([
+    [[120, 10], [120, 75]],
+    [[120, 75], [70, 65]],
+    [[70, 65], [73, 86]],
+    [[73, 86], [15, 120]],
+])
